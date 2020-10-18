fe037a335c2080b33366f315d73d2971ffef2d39
Els Dai <ed2912@columbia.edu>
2020-10-18 10:39:33 +0000

    Add export squirrel data

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,32 +1,62 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from sightings.models import Squirrel
-            
-class Command(BaseCommand):  
-    help = ("Exporting data to .csv")
+from sightings.models import Squirrel 
+
+class Command(BaseCommand):
     
     def add_arguments(self, parser):
         parser.add_argument('csv_file', type=str)
-    
-    def handle(self,*args, **options):
-        meta = {
-            'file': 'csv_file',
-            'class': Squirrel,
-            'fields': ('X', 'Y', 'Unique Squirrel ID', 'Shift', 'Date', 'Age', 'Primary Fur Color', 'Location', 'Specific Location', 'Running', 'Chasing', 'Climbing', 'Eating', 'Foraging', 'Other Activities', 'Kuks', 'Quaas', 'Moans', 'Tail flags', 'Tail twitches', 'Approaches', 'Indifferent', 'Runs from') 
-            # models fields you want to include 
-        }
-        self._write_csv(meta)
 
-    def _write_csv(self, meta):
-        """
-        :param meta: (dict) keys should be 'file' (string: absolute path), 'class' the Python class
-        object, 'fields' a list or tuple of field model field names (strings)
-        """
-        f = open(meta['file'], 'w+')
-        writer = csv.writer(f)
-        writer.writerow( meta['fields'] )
-        for obj in meta['class'].objects.all():
-            row = [unicode(getattr(obj, field)) for field in meta['fields']]
-            writer.writerow(row)
-        f.close()
+    def handle(self, *args, **options):
+        file_=options['csv_file']
+        with open(file_, 'w', newline='') as csvfile:
+            writer=csv.writer(csvfile,quoting=csv.QUOTE_ALL)
+            writer.writerow(['X',
+                        'Y', 
+                        'Unique Squirrel ID', 
+                        'Shift', 
+                        'Date', 
+                        'Age', 
+                        'Primary Fur Color', 
+                        'Location',
+                        'Specific Location',
+                        'Running',
+                        'Chasing',
+                        'Climbing',
+                        'Eating',
+                        'Foraging',
+                        'Other Activities',
+                        'Kuks',
+                        'Quaas',
+                        'Moans',
+                        'Tail flags',
+                        'Tail twitches',
+                        'Approaches',
+                        'Indifferent',
+                        'Runs from'])
+            fields = ['Latitude', 
+            	        'Longitude', 
+            		'Unique_Squirrel_ID', 
+            		'Shift', 
+            		'Date', 
+            		'Age', 
+            		'Primary_Fur_Color', 
+            		'Location',
+                        'Specific_Location',
+                        'Running',
+                        'Chasing',
+                        'Climbing',
+                        'Eating',
+                        'Foraging',
+                        'Other_Activities',
+                        'Kuks',
+                        'Quaas',
+                        'Moans',
+                        'Tail_Flags',
+                        'Tail_Twitches',
+                        'Approaches',
+                        'Indifferent',
+                        'Runs_From']
+            for obj in Squirrel.objects.all():
+                writer.writerow([getattr(obj, field) for field in fields])
--- sightings/views.py
+++ sightings/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 
 from .forms import AddForm
 
-from django.db.models import Avg, Max, Min, Count
+from django.db.models import Avg, Max, Min, Count, Q
 
 from .models import Squirrel
 
@@ -51,12 +51,32 @@ def add(request):
 
 def stats(request):
     squirrels=Squirrel.objects.all()
-    context = {'amount': len(squirrels),
-            'latitude': squirrels.aggregate(minimum=Min('Latitude'),maximum=Max('Latitude')),
-            'longitude': squirrels.aggregate(minimum=Min('Longitude'),maximum=Max('Longitude')),
-            'primary_fur_color': list(squirrels.values_list('Primary_Fur_Color').annotate(Count('Primary_Fur_Color'))),
-            'running': list(squirrels.values_list('Running').annotate(Count('Running'))),
-            'shift': list(squirrels.values_list('Shift').annotate(Count('Shift'))),
+    Age = squirrels.values_list('Age').annotate(Count('Age'))
+    Primary_Fur_Color = squirrels.values_list('Primary_Fur_Color').annotate(Count('Primary_Fur_Color'))
+    Location = squirrels.values_list('Location').annotate(Count('Location'))
+    Running = squirrels.values_list('Running').annotate(Count('Running'))
+    Chasing = squirrels.values_list('Chasing').annotate(Count('Chasing'))
+    Climbing = squirrels.values_list('Climbing').annotate(Count('Climbing'))
+    Eating = squirrels.values_list('Eating').annotate(Count('Eating'))
+    Foraging = squirrels.values_list('Foraging').annotate(Count('Foraging'))
+    Chasing = squirrels.values_list('Chasing').annotate(Count('Chasing'))
+
+    context = {'Number': len(squirrels),
+            'Latitude': squirrels.aggregate(average=Avg('Latitude')),
+            'Longitude': squirrels.aggregate(average=Avg('Longitude')),
+            'Age_Adult': Age[2][1],
+            'Age_Juvenile': Age[3][1],
+            'Black': Primary_Fur_Color[1][1],
+            'Cinnamon': Primary_Fur_Color[2][1],
+            'Gray': Primary_Fur_Color[3][1],
+            'Above_Ground': Location[1][1],
+            'Ground_Plane': Location[2][1],
+            'Running': Running[1][1],
+            'Chasing': Chasing[1][1],
+            'Climbing': Climbing[1][1],
+            'Eating': Eating[1][1],
+            'Foraging': Foraging[1][1],
+            'Chasing': Chasing[1][1],
     }
 
     return render(request, 'sightings/stats.html', context)
